9ea9054095be0b1c5ac1ba82669afbafb59e768f
arash-nouri-1 <76052712+arash-nouri-1@users.noreply.github.com>
2022-02-16 21:05:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://brave-boyd-6c3c10.netlify.app'
+        origin: '*'
     })
 );
 app.options('*', cors());
